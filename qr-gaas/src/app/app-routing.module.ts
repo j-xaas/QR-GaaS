c38cca04b833ac58fcd32dacd6c93f1cf073c0df
j-ushikoshi <j-ushikoshi@cj.jp.nec.com>
2019-11-29 16:29:35 +0900

    Generate Component

--- qr-gaas/src/app/app-routing.module.ts
+++ qr-gaas/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopComponent } from './top/top.component';
+//import { ComponentのExport} form './<component-name>/~.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [ //ここに設定したpathがURLのSuffixに入るイメージ
+  //{ path: 'top', component: <componentのexport>　Routeを設定}
+  { path: '',  redirectTo: '/top', pathMatch: 'full' }, //デフォルトルートを設定
+  { path: 'top', component: TopComponent } //Topページを設定
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //こっちにComponentのimportはいらない
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
